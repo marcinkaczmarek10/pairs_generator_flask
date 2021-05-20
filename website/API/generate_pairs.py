d61eb04de98185ad4b155087656b8b05740fbdbb
Marcin Kaczmarek <marcin@MacBook-Air-Marcin.local>
2021-05-21 00:32:25 +0300

    Rest API in progress

--- website/API/generate_pairs.py
+++ website/API/generate_pairs.py
@@ -1,28 +1,85 @@
 #from . import api
 import json
-from flask import jsonify, url_for
+from flask import jsonify, url_for, abort
 from website.database.DB import session
 from website.database.models import RandomPairsResults, RandomPairs
 from flask import Blueprint
+from website.generate_pairs.generate_random_pairs import generate_random_pairs, RandomPerson
 
 api = Blueprint('api', __name__)
+
 
 @api.route('/results/<user_id>')
 def get_results(user_id):
     query = session.query(RandomPairsResults).filter_by(user_id=user_id).all()
-    results = []
-    for each in query:
-        results.append(each.results)
-    #results_str = [str(result) for result in results]
+    results = [each.results for each in query]
     results_serialized = json.dumps(results)
-    #user_pairs = url_for(get_user_pairs)
-    print(results_serialized)
+    user_pairs = url_for('api.get_user_pairs', user_id=['user_id'])
+
     return jsonify({
         'results': results_serialized,
-        #'user_pairs': user_pairs
+        'user_pairs': user_pairs
     })
 
 
 @api.route('/pairs/<user_id>')
 def get_user_pairs(user_id):
+    query = session.query(RandomPairs).filter_by(user_id=user_id).all()
+    user_pairs = []
+    for pair in query:
+        user_pairs.append(pair.results)
+    pairs_serialized = json.dumps(user_pairs)
+    generate_pairs = url_for('api.post_generate_pairs', user_id=['user_id'], user_pairs=['user_pairs'])
+
+    return jsonify({
+        'user_pairs': pairs_serialized,
+        'generate pairs': generate_pairs
+    })
+
+
+@api.route('/generate-pairs/<user_id>', methods=['POST'])
+def post_generate_pairs(user_id, user_pairs):
+    user_random_people_pool = user_pairs
+
+    if len(user_random_people_pool) != 0 and len(user_random_people_pool) > 1:
+
+        random_people_pool = [
+            RandomPerson(row.random_person_name, row.random_person_email) for row in user_random_people_pool
+        ]
+        user_results = generate_random_pairs(random_people_pool)
+        user_random_pairs = RandomPairsResults(
+            results=user_results,
+            user_id=user_id
+        )
+        with session:
+            try:
+                session.add(user_random_pairs)
+                session.query(RandomPairs).filter_by(user_id=user_id).delete()
+                session.commit()
+            except Exception as err:
+                session.rollback()
+                print(err)
+            finally:
+                session.close()
+
+        get_results_url = url_for('api.get_results', user_id=user_id)
+        return jsonify({
+            'get_results': get_results_url
+        })
+
+    return abort(404)
+
+
+@api.route('/delete-pairs<user_id>', methods=['DELETE'])
+def delete_pairs(user_id):
+    pass
+
+
+@api.route('/delete-results<user_id>', methods=['DELETE'])
+def delete_results(user_id):
+    pass
+
+
+@api.route('/send-email<user_id>', methods=['POST'])
+def send_email_to_chosen(user_id, results):
     pass
