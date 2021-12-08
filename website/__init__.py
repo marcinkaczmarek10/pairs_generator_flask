4faf69f30a89de582d86f1ee8d3fd4e199bbbb26
Marcin Kaczmarek <marcin.kacz10@gmail.com>
2021-12-08 16:24:01 +0100

    Setting up production db to postgresql

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from website.utils.login_manager import login_manager
-from website.config import Config
+from website.config import ProductionConfig, DevelopConfig
 from website.utils.email import mail
 from website.utils.data_serializers import marshmallow
+import os
 
 
-def create_app(config_name=Config):
+if os.environ.get('ENV') == 'PRODUCTION':
+    config = ProductionConfig
+else:
+    config = DevelopConfig
+    print('THIS IS DEVELOP SERVER!')
+
+
+def create_app(config_name=config):
     app = Flask(__name__)
     app.config.from_object(config_name)
     login_manager.init_app(app)
